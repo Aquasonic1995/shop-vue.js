294d1447d2e25ff42a456702f3e6e4566d2881ea
Aquasonic1995 <Aquasonic1995@gmail.com>
2023-08-20 20:37:54 +0300

    delete files

--- shopVue/vite.config.ts
+++ shopVue/vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    open: true
+  },
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
